be7ecb2eb83bfb1b125d067c009b5161404a991c
hechao <buerchao@163.com>
2020-06-10 11:21:30 +0800

    随机字符串工具类

---
+++ common/src/main/java/com/buerc/common/utils/GenerateCodeUtil.java
@@ -0,0 +1,19 @@
+package com.buerc.common.utils;
+
+import java.util.Random;
+
+public class GenerateCodeUtil {
+    private static final String ALL_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final Integer ALL_CHAR_LENGTH = ALL_CHAR.length();
+
+    public static String code(int length) {
+        int count = 0;
+        StringBuilder pwd = new StringBuilder();
+        Random r = new Random();
+        while (count < length) {
+            pwd.append(ALL_CHAR.charAt(r.nextInt(ALL_CHAR_LENGTH)));
+            count++;
+        }
+        return pwd.toString();
+    }
+}
