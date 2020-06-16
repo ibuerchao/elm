34beaf3b958e8805e9b9a46eae74cc0306098bf3
hechao <buerchao@163.com>
2020-06-16 10:44:17 +0800

    增加部门list分页查询

--- generate/src/main/java/com/buerc/permission/mapper/SysDeptMapper.java
+++ generate/src/main/java/com/buerc/permission/mapper/SysDeptMapper.java
@@ -2,9 +2,11 @@ package com.buerc.permission.mapper;
 
 import com.buerc.permission.model.SysDept;
 import com.buerc.permission.model.SysDeptExample;
-import io.swagger.annotations.ApiModelProperty;
-import java.util.List;
+import com.buerc.sys.dto.DeptListParam;
+import com.buerc.sys.vo.DeptVo;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface SysDeptMapper {
     long countByExample(SysDeptExample example);
@@ -28,4 +30,8 @@ public interface SysDeptMapper {
     int updateByPrimaryKeySelective(SysDept record);
 
     int updateByPrimaryKey(SysDept record);
+
+    List<DeptVo> list(DeptListParam param);
+
+    long count(DeptListParam param);
 }
