9c70275cbdc91690368312538d2578649631da4f
hechao <buerchao@163.com>
2020-10-13 14:32:33 +0800

    针对拥有超级管理员角色的用户绕开权限限制

--- security/src/main/java/com/buerc/security/shiro/AuthRealm.java
+++ security/src/main/java/com/buerc/security/shiro/AuthRealm.java
@@ -7,6 +7,7 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -18,6 +19,8 @@ import java.util.Set;
 
 @Component
 public class AuthRealm extends AuthorizingRealm {
+    private static final String SUPER_ADMIN = "SUPER_ADMIN";
+
     @Resource
     private JwtTokenUtil jwtTokenUtil;
 
@@ -34,14 +37,24 @@ public class AuthRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         String token = (String) authenticationToken.getPrincipal();
         String userId = jwtTokenUtil.getUserId(token);
-        return new SimpleAuthenticationInfo(userId,token,this.getName());
+        return new SimpleAuthenticationInfo(userId, token, this.getName());
     }
 
-    private Set<String> roles(String userId){
+    @Override
+    protected boolean isPermitted(Permission permission, AuthorizationInfo info) {
+        return info.getRoles().contains(SUPER_ADMIN) || super.isPermitted(permission, info);
+    }
+
+    @Override
+    protected boolean hasRole(String roleIdentifier, AuthorizationInfo info) {
+        return info.getRoles().contains(SUPER_ADMIN) || super.hasRole(roleIdentifier, info);
+    }
+
+    private Set<String> roles(String userId) {
         return SecurityContextHolder.get(userId).getRoles();
     }
 
-    private Collection<String> permissions(String userId){
+    private Collection<String> permissions(String userId) {
         return SecurityContextHolder.get(userId).getPermissions();
     }
 }
