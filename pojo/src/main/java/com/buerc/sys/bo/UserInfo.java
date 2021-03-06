1cea5cd1a218657431d7d4ec2d61d117ac5ff00f
贺超 <hechao@kiscloud.net>
2021-03-06 16:58:25 +0800

    userInfo默认为[]而不是null

--- pojo/src/main/java/com/buerc/sys/bo/UserInfo.java
+++ pojo/src/main/java/com/buerc/sys/bo/UserInfo.java
@@ -4,6 +4,8 @@ import com.buerc.sys.vo.UserVo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -12,11 +14,11 @@ public class UserInfo {
     @ApiModelProperty(value = "用户信息")
     private UserVo info;
     @ApiModelProperty(value = "角色集合")
-    private Set<String> roles;
+    private Set<String> roles = new HashSet<>();
     @ApiModelProperty(value = "权限集合")
-    private Set<String> permissions;
+    private Set<String> permissions = new HashSet<>();
     @ApiModelProperty(value = "菜单集合")
-    private List<Menu> menus;
+    private List<Menu> menus = new ArrayList<>();
     @ApiModelProperty(value = "按钮集合")
-    private List<Button> buttons;
+    private List<Button> buttons = new ArrayList<>();
 }
